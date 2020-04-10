b51243c9f44a77d33348008705fe0e805c1833b1
EvandroArmando <otalarmando@gmail.com>
2020-04-10 15:30:19 +0100

    Estatistica
    up

---
+++ AplicativoDeEstatistica-20191221T143749Z-001/AplicativoDeEstatistica/src/BancoDeDados/BancoDeDadosGeral.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BancoDeDados;
+
+import java.sql.*;
+
+
+/**
+ *
+ * @author otala
+ */
+public class BancoDeDadosGeral {
+     public static Connection conector(){
+       java.sql.Connection  conexao = null;
+       String driver ="com.mysql.cj.jdbc.Driver";
+       String url ="jdbc:mysql://localhost:3306/jogadorespetro? useTimezone=true&serverTimezone=UTC";
+       String user ="root";
+       String password="";
+       
+        try {
+            Class.forName(driver);
+            conexao =DriverManager.getConnection(url,user,password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
